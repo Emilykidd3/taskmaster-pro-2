cb099a86286eb50c59f4feb3f2ad77c6c4f40087
emilykidd3 <e.kidd61@yahoo.com>
2021-08-30 17:14:14 -0700

    can update dates for todo lists

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,6 +87,43 @@ $("#task-form-modal").on("shown.bs.modal", function() {
   $("#modalTaskDescription").trigger("focus");
 });
 
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this).text().trim();
+
+  // create new input element
+  var dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// value of the due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this).val().trim();
+
+  // get the parent uls id attribute
+  var status = $(this).closest(".list-group").attr("id").replace("list-", "");
+
+  // get the tasks position in the list of other li elements
+  var index = $(this).closest(".list-group-item").index();
+
+  // update task in array and resave to local storage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap calsses
+  var taskSpan = $("<span>").addClass("badge-primary badge-pill").text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+})
+
 // save button in modal was clicked
 $("#task-form-modal .btn-primary").click(function() {
   // get form values
