28cf5d639d8f1f2d67f938621b0c991af42f194c
emilykidd3 <e.kidd61@yahoo.com>
2021-09-04 21:37:21 -0700

    added remove method to delete tasks when dragged to trash

--- assets/js/script.js
+++ assets/js/script.js
@@ -202,6 +202,7 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function (event, ui) {
+    ui.draggable.remove();
     console.log("drop");
   },
   over: function (event, ui) {
